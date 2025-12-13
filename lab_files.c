0628f864a20a0f9b7e11b8f89a10c101ba25cdc6
Stefan Bergman <stebeh241@gmail.com>
2025-12-13 16:01:10 +0100

    "färgit(?) med löking version men alltid något"

--- lab_files.c
+++ lab_files.c
@@ -61,19 +61,19 @@ mat_size read_matrix_size(char *filename)
 
 bool multiplication_possible(mat_size size_of_first, mat_size size_of_second) // här ska vi börja jobba
 {
-    if (size_of_first.n_rows == size_of_second.n_columns)
+    if (size_of_first.n_columns == size_of_second.n_rows)
     {
         printf("Compatible sizes so multi is possible!\n");
         return true;
     }
     else
     {
-        printf("Incompatible sizes so multi is NOT possible!\n Order matters with matrices!\n");
+        printf("Incompatible sizes so multi is NOT possible!\nOrder matters with matrices!\n");
         return false;
     }
 }
 
-matrix file_to_struct(char *mat_file_in, mat_size size_of_mat_in) // nu blev det matrix_read_data av den här ändå?
+matrix file_to_struct_mat_1(char *mat_file_in, mat_size size_of_mat_in) // nu blev det matrix_read_data av den här ändå?
 {
     int n_rows = size_of_mat_in.n_rows;
     int n_columns = size_of_mat_in.n_columns;
@@ -81,49 +81,132 @@ matrix file_to_struct(char *mat_file_in, mat_size size_of_mat_in) // nu blev det
     FILE *fp;
 
     fp = fopen(mat_file_in, "r");
-    char scrap[3];
+    char scrap[10];
     fgets(scrap, sizeof(scrap), fp);
-    for (int n = 0; n < n_rows; n++) // en iteration av nästa loop för varje rad.
+    printf("Reading first matrix from file.\nScrapping first row: %s\n", scrap); // printing scrap to see if it scraps
+    for (int n = 0; n < n_rows; n++)                                             // en iteration av nästa loop för varje rad.
     {
 
         for (int i = 0; i < n_columns; i++) // för varje column läser vi ett element och lägger på plats i structen.
         {
             fscanf(fp, "%f", &mat_out.rows[n].numbers[i]);
+            printf("mat_1: row %d number %d is %f\n", n, i, mat_out.rows[n].numbers[i]);
         }
     }
     fclose(fp);
     return mat_out;
 }
 
-matrix matrix_multiplier(matrix input_1, matrix input_2, mat_size size_of_1, mat_size size_of_2)
-// behöver nog bara ha en av deras size som input? Behöver sätta alla element till 0?
+matrix file_to_struct_mat_2(char *mat_file_in, mat_size size_of_mat_in) // nu blev det matrix_read_data av den här ändå?
+{
+    int n_rows = size_of_mat_in.n_rows;
+    int n_columns = size_of_mat_in.n_columns;
+    matrix mat_out;
+    FILE *fp;
+
+    fp = fopen(mat_file_in, "r");
+    char scrap[10];
+    fgets(scrap, sizeof(scrap), fp);
+    printf("Reading second matrix from file.\nScrapping first row: %s\n", scrap); // printing scrap to see if it scraps
+    for (int n = 0; n < n_rows; n++)                                              // en iteration av nästa loop för varje rad.
+    {
+
+        for (int i = 0; i < n_columns; i++) // för varje column läser vi ett element och lägger på plats i structen.
+        {
+            fscanf(fp, "%f", &mat_out.columns[i].numbers[n]);
+            printf("mat_1: column %d number %d is %f\n", i, n, mat_out.columns[i].numbers[n]);
+        }
+    }
+    fclose(fp);
+    return mat_out;
+}
+
+matrix matrix_multiplier(matrix input_1, matrix input_2, mat_size size_of_1, mat_size size_of_2) // takes two "matrices" and their sizes and returns a matrix with new size.
 {
     matrix output;
     for (int i = 0; i < size_of_1.n_rows; i++)
     {
-        float row_1_sum = 0;
-        for (int n = 0; n < size_of_1.n_columns; n++)
+        for (int j = 0; j < size_of_2.n_columns; j++)
         {
-            output.rows[i].numbers[i] = 0;
-            float product = input_1.rows[i].numbers[i] * input_2.columns[i].numbers[i];
-            output.rows[i].numbers[i] = output.rows[i].numbers[i] + product;
+            output.rows[i].numbers[j] = 0;
+            for (int n = 0; n < size_of_1.n_columns; n++)
+            {
+                float product = input_1.rows[i].numbers[n] * input_2.columns[j].numbers[n];
+                output.rows[i].numbers[j] = output.rows[i].numbers[j] + product;
+            }
         }
     }
     return output;
 }
 
-void write_result_file(matrix result)
+void write_result_file(char *filename, matrix result, mat_size result_size, int rows_of_A, int columns_of_B)
 {
+    printf("Resulting matrix will be %d rows and %d columnes\n", rows_of_A, columns_of_B);
+    FILE *fp;
+    fp = fopen(filename, "w");
+    fprintf(fp, "%d %d\n", rows_of_A, columns_of_B);
+    for (int n = 0; n < rows_of_A; n++) // en iteration av nästa loop för varje rad.
+    {
+
+        for (int i = 0; i < columns_of_B; i++) // för varje column skriver vi ett element.
+        {
+            printf("out: rad %d column %d = %f,\n", n, i, result.rows[n].numbers[i]);
+            fprintf(fp, "%f ", result.rows[n].numbers[i]);
+        }
+        fprintf(fp, "\n");
+    }
+    fclose(fp);
+}
+
+void print_struct_mat(matrix input, mat_size input_size)
+{
+    for (int n = 0; n < input_size.n_rows; n++) // en iteration av nästa loop för varje rad.
+    {
+
+        for (int i = 0; i < input_size.n_columns; i++) // för varje column skriver vi ett element.
+        {
+            printf("%f", input.rows[n].numbers[i]);
+        }
+    }
 }
 
 int main()
 {
-    // create_file("mat1.txt");
-    // write_to_file("mat1.txt", "3 4");
-    mat_size size_of_A = read_matrix_size("A.mat");
-    mat_size size_of_B = read_matrix_size("B.mat");
-    multiplication_possible(size_of_A, size_of_B);
-    matrix A = file_to_struct("A.mat", size_of_A);
-    matrix B = file_to_struct("B.mat", size_of_B);
-    matrix_multiplier(A, B, size_of_A, size_of_B);
+    int run = 1;
+    while (run == 1)
+    {
+        printf("* This is a simple matrix multiplication tool\n* Input is two files with format pascalmatrix\n* Press 1 to start or 0 to exit.\n");
+        printf("\n");
+        scanf("%d", &run);
+        if (run == 1)
+        {
+            printf("Choose name of first file, it needs to be in current folder\n");
+            char path_to_A[256];
+            scanf("%s", path_to_A);
+            printf("Choose second file, it needs to be in current folder\n");
+            char path_to_B[256];
+            scanf("%s", path_to_B);
+            printf("Choose name of output file, it will be saved in current folder\n");
+            char result_name[256];
+            scanf("%s", result_name);
+            // create_file("mat1.txt");
+            // write_to_file("mat1.txt", "3 4");
+            mat_size size_of_A = read_matrix_size(path_to_A);
+            mat_size size_of_B = read_matrix_size(path_to_B);
+            if (multiplication_possible(size_of_A, size_of_B))
+            {
+                matrix A = file_to_struct_mat_1("A.mat", size_of_A);
+                matrix B = file_to_struct_mat_2("B.mat", size_of_B);
+                matrix result = matrix_multiplier(A, B, size_of_A, size_of_B);
+                write_result_file(result_name, result, size_of_A, size_of_A.n_rows, size_of_B.n_columns);
+            }
+            printf("********************************************************\n");
+            printf("MULTIPLICATION COMPLETE, RESULT IS STORED IN %s\n", result_name);
+            printf("********************************************************\n");
+        }
+        else
+        {
+            run = 0;
+        }
+    }
 }
